0688d5c8789da38bfca864a06ac4754430dd1697
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-01-04 23:19:37 -0800

    feat: add tooltip click method

--- src/components/tooltip/index.ts
+++ src/components/tooltip/index.ts
@@ -1,7 +1,52 @@
 import { reactive } from '@esportsplus/reactivity';
 import { html } from '@esportsplus/template';
+import events from '@esportsplus/delegated-events';
 import menu from './menu';
 
+
+let initialized = false,
+    root: { active: boolean }[] = [];
+
+
+const onclick = ({ active, toggle }: { active?: boolean, toggle?: boolean }) => {
+    if (!initialized) {
+        events.register(document.body, 'click', () => {
+            if (!root.length) {
+                return;
+            }
+
+            let deactivate = root.splice(0);
+
+            for (let i = 0, n = deactivate.length; i < n; i++) {
+                deactivate[i].active = false;
+            }
+        });
+        initialized = true;
+    }
+
+    let state = reactive({
+            active: active || false
+        });
+
+    return html({
+        class: () => {
+            return `tooltip ${state.active ? '--active' : ''}`;
+        },
+        onclick: function(this: HTMLElement, e: Event) {
+            let active = true;
+
+            if (toggle && e.target && this.isSameNode(e.target as Node)) {
+                active = !state.active;
+            }
+
+            state.active = active;
+
+            if (active) {
+                root.push(state);
+            }
+        }
+    });
+};
 
 const onfocus = (active: boolean = false) => {
     let state = reactive({ active });
@@ -36,4 +81,4 @@ const onhover = (active: boolean = false) => {
 };
 
 
-export default { onfocus, onhover, menu };
+export default { onclick, onfocus, onhover, menu };
