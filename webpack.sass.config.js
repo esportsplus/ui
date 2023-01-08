0ad58175915208923d5f425ca73f035ebab2d3b6
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-01-07 22:20:30 -0800

    refactor: use esportsplus/app dependency

--- webpack.sass.config.js
+++ webpack.sass.config.js
@@ -1,101 +1,14 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const autoprefixer = require('autoprefixer');
-const glob = require('glob');
-const path = require('path');
-const sass = require('sass');
+const { config, entry: e } = require('@esportsplus/app/webpack.sass.config.js');
 
 
-const config = (entry, output, production) => {
-    output = path.resolve(output).replace(/\\/g, '/');
-    production = production !== 'false' ? true : false;
+let entry = {
+        'components': e('./src/components/**/index.scss'),
+        'components.variables': e('./src/components/**/variables.scss'),
 
-    return {
-        entry,
-        mode: (production ? 'production' : 'development'),
-        module: {
-            rules: [
-                {
-                    test: /\.(c|sc|sa)ss$/,
-                    use: [
-                        {
-                            loader: MiniCssExtractPlugin.loader,
-                            options: {
-                                esModule: false,
-                            },
-                        },
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                // Prevents Following Urls To Fonts/Images
-                                url: false,
-                                importLoaders: 1
-                            }
-                        },
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                postcssOptions: {
-                                    plugins: [
-                                        autoprefixer()
-                                    ]
-                                }
-                            }
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                // Use `dart-sass`
-                                implementation: sass,
-                            },
-                        },
-                    ],
-                },
-            ],
-        },
-        optimization: {
-            minimize: production,
-            minimizer: [
-                `...`,
-                new CssMinimizerPlugin()
-            ]
-        },
-        output: {
-            path: output,
-        },
-        plugins: [
-            new MiniCssExtractPlugin(),
-            new CleanWebpackPlugin({
-                cleanAfterEveryBuildPatterns: [`${output}/**/*.js`],
-                cleanOnceBeforeBuildPatterns: [],
-                dangerouslyAllowCleanPatternsOutsideProject: false,
-                dry: false,
-                verbose: false
-            })
-        ],
-        watch: !production
-    };
-};
-
-const scss = (pattern, { normalizer, ui } = {}) => {
-    let scss = glob.sync(path.resolve(pattern).replace(/\\/g, '/'), { nosort: true });
-
-    if (ui !== undefined) {
-        if (typeof ui !== 'string') {
-            throw new Error('`ui` must be a string');
-        }
-
-        scss.push(`@esportsplus/ui/build/css/css-utilities${str ? `.${str}` : ''}.scss`);
-        scss.unshift(`@esportsplus/ui/build/css/components${str ? `.${str}` : ''}.scss`);
-    }
-
-    if (normalizer) {
-        scss.unshift('modern-normalize/modern-normalize.css');
-    }
-
-    return scss.flat();
-};
+        'css-utilities': e('./src/css-utilities/**/index.scss'),
+        'css-utilities.variables': e('./src/css-utilities/**/variables.scss')
+    },
+    output = './build/css';
 
 
-module.exports = { config, scss };
+module.exports = ({ production }) => config(entry, output, production);
