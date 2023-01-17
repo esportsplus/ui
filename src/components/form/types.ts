57cc3126f606c151989c7cb3d8d7e54ab980c0e3
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-01-17 02:59:44 -0800

    fix: types

--- src/components/form/types.ts
+++ src/components/form/types.ts
@@ -1,9 +1,16 @@
-import { Response } from '@esportsplus/action';
+import { response, Response } from '@esportsplus/action';
+import alert from '~/components/alert';
 
 
-type Action = <T>(data: T) => Promise<Errors> | Errors;
+type Action = (data: Payload) => Promise<Errors> | Errors;
 
 type Errors = { errors: Response<unknown>['errors'] };
 
+type Payload = {
+    alert: typeof alert;
+    input: Record<string, any>;
+    response: typeof response;
+};
 
-export { Action, Errors };
+
+export { Action, Errors, Payload };
