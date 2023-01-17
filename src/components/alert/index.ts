4de9ee268e858e82670aaa97e43a84f1beba1c3f
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-01-17 07:37:03 -0800

    remove processing from alert

--- src/components/alert/index.ts
+++ src/components/alert/index.ts
@@ -13,7 +13,6 @@ let modifiers: Record<Type, string> = {
     state = reactive({
         active: false,
         messages: new Set as Set<string>,
-        processing: false,
         seconds: 0,
         state: 'activating',
         type: '' as Type
@@ -42,7 +41,6 @@ function activate(key: Type, messages: string | string[], seconds: number = 0) {
     // Slide in animation needs time
     if (state.active) {
         state.active = true;
-        state.processing = false;
 
         if (seconds) {
             if (!state.seconds) {
@@ -70,7 +68,6 @@ function activate(key: Type, messages: string | string[], seconds: number = 0) {
 
     setTimeout(() => {
         state.active = true;
-        state.processing = false;
 
         if (seconds) {
             if (!state.seconds) {
@@ -102,7 +99,6 @@ function deactivate() {
     setTimeout(() => {
         state.active = false;
         state.messages.clear();
-        state.processing = false;
     }, timeout);
 }
 
@@ -111,37 +107,25 @@ const error = (messages: string | string[], seconds: number = 0) => activate('er
 
 const info = (messages: string | string[], seconds: number = 0) => activate('info', messages, seconds);
 
-const processing = () => {
-    state.state = 'activating';
-    state.processing = true;
-
-    // Slide in animation needs time
-    setTimeout(() => {
-        state.active = true;
-    }, timeout);
-};
-
 const success = (messages: string | string[], seconds: number = 0) => activate('success', messages, seconds);
 
 
 const h = () => {
-    return () => state.active || state.processing ? html`
+    return () => state.active ? html`
         <div class='alert anchor anchor--ne ${() => state.active && '--active'} ${() => `alert--${state.state}`}'>
-            ${() => !state.processing ? html`
-                <div class="alert-close --flex-start --margin-right --margin-100" onclick='${deactivate}'>
-                    <div class='button --background-state ${() => `--background-${modifiers[state.type] || 'black'}`} --color-state --color-white --flex-center --padding-300'>
-                        <div class="icon --size-300">
-                            <svg width="16" height="16" viewBox="0 0 16 16">
-                                <path d="M3.527 14.948a.176.176 0 01-.248 0L1.051 12.72a.176.176 0 010-.248l11.42-11.419a.176.176 0 01.248 0l2.229 2.228a.174.174 0 010 .248L3.527 14.948z"/>
-                                <path d="M12.472 14.948c.068.068.18.068.248 0l2.229-2.229a.176.176 0 000-.248L3.528 1.052a.176.176 0 00-.248 0L1.052 3.28a.176.176 0 000 .248l11.42 11.42z"/>
-                            </svg>
-                        </div>
+            <div class="alert-close --flex-start --margin-right --margin-100" onclick='${deactivate}'>
+                <div class='button --background-state ${() => `--background-${modifiers[state.type] || 'black'}`} --color-state --color-white --flex-center --padding-300'>
+                    <div class="icon --size-300">
+                        <svg width="16" height="16" viewBox="0 0 16 16">
+                            <path d="M3.527 14.948a.176.176 0 01-.248 0L1.051 12.72a.176.176 0 010-.248l11.42-11.419a.176.176 0 01.248 0l2.229 2.228a.174.174 0 010 .248L3.527 14.948z"/>
+                            <path d="M12.472 14.948c.068.068.18.068.248 0l2.229-2.229a.176.176 0 000-.248L3.528 1.052a.176.176 0 00-.248 0L1.052 3.28a.176.176 0 000 .248l11.42 11.42z"/>
+                        </svg>
                     </div>
                 </div>
-            ` : ''}
+            </div>
 
             <div class="card --overflow-hidden" style='--background: var(--color-white-400)'>
-                <div class="alert-message ${() => !state.processing && '--active'} --flex-row --padding --padding-horizontal-500 --padding-vertical-400">
+                <div class="alert-message --active --flex-row --padding --padding-horizontal-500 --padding-vertical-400">
                     <div class='--flex-row --flex-fill --flex-vertical'>
                         <div class="--flex-fill --flex-column --padding-right --padding-400">
                             <h5 class="page-title">
@@ -150,28 +134,6 @@ const h = () => {
                             ${() => state.type && [...state.messages].map((message) => html`
                                 <p class='--margin-top --margin-border-width-500'>${message}</p>
                             `)}
-                        </div>
-
-                        ${() => !state.processing && state.seconds ? html`
-                            <svg class='alert-timer' style='--animation-duration: ${state.seconds}s;'>
-                                <circle class="alert-timer-bg" cx="50%" cy="50%" r="40%" style='--border-color: var(--color-grey-500);' />
-                                <circle class="alert-timer-meter" cx="50%" cy="50%" r="40%" style='--border-color: var(--color-black-300);' />
-                            </svg>
-                        ` : ''}
-                    </div>
-                </div>
-
-                <div class='alert-processing ${() => state.processing && '--active'} --flex-row --flex-vertical --padding --padding-500'>
-                    <div class="processing">
-                        <span class='processing-ring'></span>
-                    </div>
-                    <div class='text --flex-fill --margin-left --margin-300 --text-bold' style='--color: var(--color-text-500)'>
-                        Processing
-
-                        <div class='ellipsis'>
-                            <span class='ellipsis-dot --margin-left --margin-border-width'>.</span>
-                            <span class='ellipsis-dot --margin-left --margin-border-width'>.</span>
-                            <span class='ellipsis-dot --margin-left --margin-border-width'>.</span>
                         </div>
                     </div>
                 </div>
@@ -183,4 +145,4 @@ const h = () => {
 const types = ['error', 'info', 'success'] as const;
 
 
-export default { deactivate, error, html: h, info, processing, success, types };
+export default { deactivate, error, html: h, info, success, types };
