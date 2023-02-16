c06ec3acf2c4212963e0fe8f9b0937b0993ad703
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-02-16 04:13:24 -0800

    fix class assignment

--- src/components/field/optional.ts
+++ src/components/field/optional.ts
@@ -4,21 +4,23 @@ import tex from './text';
 
 
 const select = (data: Parameters<typeof s>[0] & { field: Parameters<typeof sel>[0] }) => {
-    return s(Object.assign(data, {
-        field: {
-            class: `field--optional ${data?.class || ''}`,
-            content: sel( data.field )
-        }
-    }));
+    data.field.content = sel(
+        Object.assign(data.field || {}, {
+            class: `field--optional ${data?.field?.class || ''}`
+        })
+    );
+
+    return s(data);
 };
 
 const text = (data: Parameters<typeof s>[0] & { field: Parameters<typeof tex>[0] }) => {
-    return s(Object.assign(data, {
-        field: {
-            class: `field--optional ${data?.class || ''}`,
-            content: tex( data.field )
-        }
-    }));
+    data.field.content = tex(
+        Object.assign(data.field || {}, {
+            class: `field--optional ${data?.field?.class || ''}`
+        })
+    );
+
+    return s(data);
 };
 
 
