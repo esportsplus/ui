196c1183e79b97d1bd88235108045e17f3ae53c4
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-02-18 00:40:12 -0800

    export textarea

--- src/components/field/index.ts
+++ src/components/field/index.ts
@@ -3,7 +3,8 @@ import file from './file';
 import optional from './optional';
 import select from './select';
 import s from './switch';
+import textarea from './textarea';
 import text from './text';
 
 
-export default { checkbox, file, optional, select, switch: s, text };
+export default { checkbox, file, optional, select, switch: s, textarea, text };
