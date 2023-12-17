0a484733bec695573233968a8a472b49b653d609
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-12-17 05:11:10 -0800

    chore: cleanup

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as alert }from './alert';
 export { default as field }from './field';
 export { default as form }from './form';
-export { default as overlay }from './overlay';
 export { default as page }from './page';
 export { default as root }from './root';
 export { default as scrollbar }from './scrollbar';
