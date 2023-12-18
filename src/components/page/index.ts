3e3959fb28e6286ec772b45f790313f781e75dba
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-12-17 17:10:09 -0800

    feat: testing shared props

--- src/components/page/index.ts
+++ src/components/page/index.ts
@@ -1,26 +1,22 @@
-import { html } from '@esportsplus/template';
+const subtitle = () => {
+    return {
+        class: 'page-subtitle --margin-top --margin-300 --text-crop-bottom'
+    };
+};
+
+const suptitle = () => {
+    return {
+        class: 'page-suptitle --text-bold --text-crop --text-uppercase --text-200',
+        style: '--color-default: var(--color-primary-400);letter-spacing: 0.24px;'
+    }
+};
+
+const title = (subtitle: unknown, suptitle: unknown) => {
+    return {
+        class: `page-title --line-height-200 --margin-300 ${!subtitle && '--text-crop-bottom'} ${suptitle ? '--margin-top' : '--text-crop-top'}`
+    };
+};
 
 
-const header = ({ subtitle, suptitle, title }: { subtitle?: string, suptitle?: string, title?: string }) => html`
-    ${suptitle ? html`
-        <span class="page-suptitle --text-bold --text-crop --text-uppercase --text-200" style="--color-default: var(--color-primary-400);letter-spacing: 0.24px;">
-            ${suptitle}
-        </span>
-    ` : ''}
-
-    ${title ? html`
-        <h1 class="page-title --line-height-200 --margin-300 ${!subtitle && '--text-crop-bottom'} ${suptitle ? '--margin-top' : '--text-crop-top'}">
-            ${title}
-        </h1>
-    ` : ''}
-
-    ${subtitle ? html`
-        <span class="page-subtitle --margin-top --margin-300 --text-crop-bottom">
-            ${subtitle}
-        </span>
-    ` : ''}
-`;
-
-
-export default { header };
-export { header };
+export default { subtitle, suptitle, title };
+export { subtitle, suptitle, title };
