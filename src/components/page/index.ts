0ccaa3bc93a29e8022846c7ac51846ea06422f18
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-12-18 09:27:55 -0800

    feat: shift to css:has

--- src/components/page/index.ts
+++ src/components/page/index.ts
@@ -1,5 +1,5 @@
 const subtitle = {
-    class: 'page-subtitle --margin-top --margin-200 --text-crop-bottom'
+    class: 'page-subtitle --margin-200 --text-crop-bottom'
 };
 
 const suptitle = {
@@ -7,7 +7,7 @@ const suptitle = {
 };
 
 const title = {
-    class: (subtitle?: boolean, suptitle?: boolean) => `page-title --line-height-200 --margin-300 ${!subtitle && '--text-crop-bottom'} ${suptitle ? '--margin-top' : '--text-crop-top'}`
+    class: 'page-title --line-height-200 --margin-400 --text-crop'
 };
 
 
