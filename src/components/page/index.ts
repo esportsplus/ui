0b459e22eb9bb4cee8df9ef72d318fbaaecc8e7f
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-12-17 16:10:13 -0800

    chore: cleanup

--- src/components/page/index.ts
+++
@@ -1,6 +0,0 @@
-import header from './header';
-import layout from './layout';
-
-
-export default { header, layout };
-export { header, layout };
--- src/components/page/layout.ts
+++
@@ -1,8 +0,0 @@
-import { html } from '@esportsplus/template';
-
-
-export default (data: { class?: string, content?: any } = {}) => html`
-    <section class='page ${data?.class}'>
-        ${data?.content}
-    </section>
-`;
