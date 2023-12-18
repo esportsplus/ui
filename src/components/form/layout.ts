806fc47bc99511a28a90914121ae245e455830e7
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-12-17 16:11:37 -0800

    fix

--- src/components/form/layout.ts
+++ src/components/form/layout.ts
@@ -1,5 +1,4 @@
 import { html } from '@esportsplus/template';
-import { header } from '~/components/page';
 
 
 type Data = {
@@ -11,12 +10,10 @@ type Data = {
     };
     class?: string;
     content?: any;
-} & Parameters<typeof header>[0];
+};
 
 
 export default (data: Data) => html`
-    ${header(data)}
-
     <form class='${data?.class}' ${data?.action || ''}>
         ${data?.content || ''}
 
