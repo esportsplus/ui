0caad7658ec0fce079b4e435228103569f470522
Israel Casillas Jr <ICJR@users.noreply.github.com>
2023-12-18 10:09:00 -0800

    fix

--- src/components/page/index.ts
+++ src/components/page/index.ts
@@ -7,7 +7,7 @@ const suptitle = {
 };
 
 const title = {
-    class: (subtitle?: boolean, suptitle?: boolean) => `page-title --line-height-200 --margin-400 ${!subtitle && '--text-crop-bottom'} ${suptitle && '--text-crop-top'}`
+    class: (subtitle?: boolean, suptitle?: boolean) => `page-title --line-height-200 --margin-400 ${!subtitle && '--text-crop-bottom'} ${!suptitle && '--text-crop-top'}`
 };
 
 
