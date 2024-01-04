337a67fc7ed546b83775fa3c017fec5221a12156
Israel Casillas Jr <ICJR@users.noreply.github.com>
2024-01-04 15:58:58 -0800

    chore: revert exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-// Required for `tsc-alias` to work
-// - `package.json` exports makes this file innaccesible to avoid barrel imports
-export { default as alert }from './components/alert';
-export { default as field }from './components/field';
-export { default as form }from './components/form';
-export { default as number }from './components/number';
-export { default as page }from './components/page';
-export { default as root }from './components/root';
-export { default as scrollbar }from './components/scrollbar';
-export { default as site }from './components/site';
-export { default as tooltip }from './components/tooltip';
+import '@esportsplus/webpack/global.d.ts';
+export * from './components';
--- webpack.config.ts
+++ webpack.config.ts
@@ -5,19 +5,26 @@ export default ({ production }: { production?: string }) => {
     return config.web({
         contenthash: false,
         entry: {
-            components: {
-                styles: entry.css('src/components/**/index.scss'),
-                variables: entry.css('src/components/**/variables.scss')
-            },
-            fonts: {
-                montserrat: entry.css('storage/fonts/montserrat/index.css')
-            },
-            normalizer: entry.css('modern-normalize/modern-normalize.css'),
-            utilities: {
-                styles: entry.css('src/css-utilities/**/index.scss'),
-                variables: entry.css('src/css-utilities/**/variables.scss')
+            css: {
+                components: {
+                    styles: entry.css('src/components/**/index.scss'),
+                    variables: entry.css('src/components/**/variables.scss')
+                },
+                fonts: {
+                    montserrat: entry.css('storage/fonts/montserrat/index.css')
+                },
+                normalizer: entry.css('modern-normalize/modern-normalize.css'),
+                utilities: {
+                    styles: entry.css('src/css-utilities/**/index.scss'),
+                    variables: entry.css('src/css-utilities/**/variables.scss')
+                }
             }
         },
         mode: production !== 'false' ? 'production': 'development',
+        // Temporary output until css root directory can be set through
+        // package.json or similar ( like 'main' key )
+        output: {
+            path: '.'
+        }
     });
 };
