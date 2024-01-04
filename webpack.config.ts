5b5a2c2799c42be25c338ee30faedd256c63e576
Israel Casillas Jr <ICJR@users.noreply.github.com>
2024-01-04 15:38:10 -0800

    chore: cleanup

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,24 +5,19 @@ export default ({ production }: { production?: string }) => {
     return config.web({
         contenthash: false,
         entry: {
-            css: {
-                components: {
-                    styles: entry.css('src/components/**/index.scss'),
-                    variables: entry.css('src/components/**/variables.scss')
-                },
-                fonts: {
-                    montserrat: entry.css('storage/fonts/montserrat/index.css')
-                },
-                normalizer: entry.css('modern-normalize/modern-normalize.css'),
-                utilities: {
-                    styles: entry.css('src/css-utilities/**/index.scss'),
-                    variables: entry.css('src/css-utilities/**/variables.scss')
-                }
+            components: {
+                styles: entry.css('src/components/**/index.scss'),
+                variables: entry.css('src/components/**/variables.scss')
+            },
+            fonts: {
+                montserrat: entry.css('storage/fonts/montserrat/index.css')
+            },
+            normalizer: entry.css('modern-normalize/modern-normalize.css'),
+            utilities: {
+                styles: entry.css('src/css-utilities/**/index.scss'),
+                variables: entry.css('src/css-utilities/**/variables.scss')
             }
         },
         mode: production !== 'false' ? 'production': 'development',
-        output: {
-            path: '.'
-        },
     });
 };
