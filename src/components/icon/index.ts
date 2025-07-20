4bf7572fa3c37417c9b691e771d651ceab03a71b
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-07-20 03:17:46 -0700

    fix: icon export

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,1 +1,11 @@
+import { html, svg } from '@esportsplus/template';
 import './scss/index.scss';
+
+
+export default (attributes: Record<string, unknown>, icon: Parameters<typeof svg.sprite>[0]) => {
+    return html`
+        <div class='icon' ${attributes}>
+            ${svg.sprite(icon)}
+        </div>
+    `;
+};
