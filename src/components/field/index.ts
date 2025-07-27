47435e8bc6e450245595f9998753817eae996302
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-07-27 07:57:20 -0700

    fix: field exports

--- src/components/field/index.ts
+++ src/components/field/index.ts
@@ -1,10 +1,15 @@
 import checkbox from './checkbox';
 import description from './description';
-import select from './select';
 import input from './input';
+import select from './select';
 import title from './title';
 import './scss/index.scss';
 
 
-// export default { checkbox, description, file, select, switch: s, textarea, text, title };
-// export { checkbox, description, file, select, s as switch, textarea, text, title };
+const { checkbox: cb, radio, switch: sw } = checkbox;
+
+const { file, text, textarea } = input;
+
+
+export default { checkbox: cb, description, file, radio, select, switch: sw, textarea, text, title };
+export { cb as checkbox, description, file, radio, select, sw as switch, textarea, text, title };
