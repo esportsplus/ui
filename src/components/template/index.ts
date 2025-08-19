904c28a4e0c83bb207d15038e0cc7f330c09517c
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-08-19 04:35:18 -0700

    fix: types

--- src/components/template/index.ts
+++ src/components/template/index.ts
@@ -3,15 +3,15 @@ import { Attributes, Renderable } from '@esportsplus/template';
 
 const factory = <
     A extends Attributes,
-    C = Renderable<unknown>,
+    C = Renderable<any>,
     Context = { attributes?: A, content?: C }
 >(
-    template: (this: Context, attributes: Readonly<A>, content: C) => Renderable<unknown>
+    template: (this: Context, attributes: Readonly<A>, content: C) => Renderable<any>
 ) => {
-    function factory(): Renderable<unknown>;
-    function factory(content: C): Renderable<unknown>;
-    function factory(attributes: A, content: C): Renderable<unknown>;
-    function factory(this: Context, one?: A | C, two?: C): Renderable<unknown> {
+    function factory(): ReturnType<typeof template>;
+    function factory(content: C): ReturnType<typeof template>;
+    function factory(attributes: A, content: C): ReturnType<typeof template>;
+    function factory(this: Context, one?: A | C, two?: C): ReturnType<typeof template> {
         let attributes: A = {} as A,
             content: C;
 
