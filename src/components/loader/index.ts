bc69ecda9a1dbc5b7295f79417922f4fc50f3787
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-08-26 03:13:31 -0700

    chore: fix loader

--- src/components/loader/index.ts
+++ src/components/loader/index.ts
@@ -1,43 +1,76 @@
 import { reactive } from '@esportsplus/reactivity';
-import { html } from '@esportsplus/template';
+import { html, Attributes } from '@esportsplus/template';
+import { omit } from '@esportsplus/utilities'
 import template from '~/components/template';
 import './scss/index.scss';
 
 
+type A = Attributes & {
+    'loader-content'?: Attributes,
+    'loader-logo'?: Attributes
+};
+
+
+const OMIT = ['loader-content', 'loader-logo'];
+
+
 export default template.factory(
-    (attributes, content) => {
-        let c = () => state.load && 'loader--load',
+    (attributes: A, content) => {
+        let a = {
+                class: () => state.load && 'loader--load'
+            },
             state = reactive({
                 load: false,
+                remove: false,
                 scale: false
             });
 
-        setTimeout(() => {
-            state.scale = true;
-        }, 300);
+        return () => {
+            if (state.remove) {
+                return;
+            }
 
-        return html`
-            <div class='loader' ${{ class: c }}>
-                <div class='loader' ${{ class: c }}>
-                    <div class='loader-content'>
-                        <div
-                            class='loader-logo text --flex-center --text-uppercase --text-600'
-                            style='color: var(--color-grey-500);'
-                            ${attributes}
-                            ${{
-                                class: () => state.scale && 'loader-logo--scale',
-                                onanimationend: ({ animationName: name }) => {
-                                    if (name === 'scale') {
-                                        state.load = true;
-                                    }
-                                }
-                            }}
-                        >
-                            ${content}
-                        </div>
+            let i = 0;
+
+            return html`
+                <div
+                    class='loader'
+                    onanimationend=${(e: AnimationEvent) => {
+                        i++;
+
+                        if (e.animationName === 'move' && i > 1) {
+                            state.remove = true;
+                        }
+                    }}
+                    ${a}
+                    ${omit(attributes, OMIT)}
+                >
+                    <div class='loader' ${a}>
+                        ${content && html`
+                            <div class='loader-content' ${attributes['loader-content']}>
+                                <div
+                                    class='loader-logo text --flex-center --text-uppercase --text-600'
+                                    style='color: var(--color-grey-500);'
+                                    ${attributes['loader-logo']}
+                                    ${{
+                                        class: () => state.scale && 'loader-logo--scale',
+                                        onanimationend: ({ animationName: name }) => {
+                                            if (name === 'scale') {
+                                                state.load = true;
+                                            }
+                                        },
+                                        onconnect: () => {
+                                            state.scale = true;
+                                        }
+                                    }}
+                                >
+                                    ${content}
+                                </div>
+                            </div>
+                        `}
                     </div>
                 </div>
-            </div>
-        `;
+            `;
+        }
     }
 );
