b05383c030f852a034878a0df7589babcce7c6a6
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-10-28 10:17:16 -0700

    fix: back component

--- src/components/back/index.ts
+++ src/components/back/index.ts
@@ -1,11 +1,14 @@
-import { html, Attributes, Renderable } from '@esportsplus/template';
+import { html } from '@esportsplus/template';
+import template from '~/components/template';
 import './scss/index.scss';
 
 
-export default ({ attributes, content }: { attributes?: Attributes, content?: Renderable<any> }) => {
-    return html`
-        <a class='back link --padding-0px --flex-vertical' ${attributes}>
-            ${content}
-        </a>
-    `;
-};
+export default template.factory(
+    (attributes, content) => {
+        return html`
+            <a class='back link --padding-0px --flex-vertical' ${attributes}>
+                ${content}
+            </a>
+        `;
+    }
+);
