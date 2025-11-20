c4605f4165d4c955b2f38e2b2114af74739c0a0c
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-11-19 20:42:15 -0800

    refactor: simple component themes to css selectors

--- src/themes/dark/index.ts
+++ src/themes/dark/index.ts
@@ -1,7 +1,5 @@
 export { default as alert } from './alert';
 export { default as back } from './back';
-export { default as button } from './button';
 export { default as input } from './input';
-export { default as link } from './link';
 export { default as select } from './select';
 export { default as textarea } from './textarea';
