f49455ef78c3cc5d0d126273d5374bd98ca0ace5
Israel Casillas Jr <ICJR@users.noreply.github.com>
2026-07-19 02:56:22 -0700

    refactor(accordion): grid-rows collapse via inert

--- src/components/accordion/index.ts
+++ src/components/accordion/index.ts
@@ -7,44 +7,25 @@ import './scss/index.scss';
 
 type A = Attributes & { state?: { active: boolean | number } };
 
-type Accordion = HTMLElement & { [key: symbol]: { active: boolean | number } };
-
 
 const OMIT = ['state'];
 
 
-let key = Symbol();
-
-
 export default template.factory(
     function(attributes: A, content) {
-        let ref: Accordion,
-            state = attributes.state || reactive({
+        let state = attributes.state || reactive({
                 active: 0
             });
 
         return html`
             <div
+                class='accordion'
                 ${omit(attributes, OMIT)}
                 ${{
-                    class: () => {
-                        return state.active && '--active';
-                    },
-                    onrender: (element) => {
-                        ( ref = element as Accordion )[key] = state;
-                    },
-                    style: () => {
-                        let parent = ref.closest<Accordion>('accordion');
-
-                        if (parent && key in parent) {
-                            parent[key].active = (+parent[key].active) + 1;
-                        }
-
-                        return state.active && `--max-height: ${ref.scrollHeight}`;
-                    }
+                    inert: () => !state.active
                 }}
             >
-                ${content}
+                <div class='accordion-content'>${content}</div>
             </div>
         `;
     }
