7a03cf65ff2608020a451d82ea26b220238ed672
Israel Casillas Jr <ICJR@users.noreply.github.com>
2026-07-20 07:39:34 -0700

    refactor(accordion): require state prop

--- src/components/accordion/index.ts
+++ src/components/accordion/index.ts
@@ -1,21 +1,18 @@
 import { html, Attributes } from '@esportsplus/template';
-import { reactive } from '@esportsplus/reactivity';
 import { omit } from '@esportsplus/utilities';
 import template from '~/components/template';
 import './scss/index.scss';
 
 
-type A = Attributes & { state?: { active: boolean | number } };
+type A = Attributes & { state: { active: boolean | number } };
 
 
 const OMIT = ['state'];
 
 
-export default template.factory(
+export default template.factory<A>(
     function(attributes: A, content) {
-        let state = attributes.state || reactive({
-                active: 0
-            });
+        let state = attributes.state;
 
         return html`
             <div
