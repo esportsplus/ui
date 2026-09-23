0c6a30347229cce7377df6232a5a560946ee9519
Israel Casillas Jr <ICJR@users.noreply.github.com>
2026-09-22 21:37:26 -0700

    refactor(form): make input state accessor generic
    Components can store and read richer state than the error field.

--- src/components/form/input.ts
+++ src/components/form/input.ts
@@ -1,18 +1,19 @@
 import { Element } from '@esportsplus/template';
 
 
+// TODO: Needs to be expanded and we need better type for template
 const KEY = Symbol();
 
 
-const get = (element?: Element) => {
+const get = <T extends { error: string }>(element?: Element) => {
     if (element) {
-        return element[KEY] as { error: string } | undefined;
+        return element[KEY] as T | undefined;
     }
 
     return undefined;
 };
 
-const onrender = (reactive: { error: string }) => {
+const onrender = <T extends { error: string }>(reactive: T) => {
     return (element: Element) => {
         element[KEY] = reactive;
     };
