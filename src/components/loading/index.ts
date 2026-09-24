e3fa01171db20a8770ce802abc8aa14acf6b1e18
Israel Casillas Jr <ICJR@users.noreply.github.com>
2026-09-23 23:58:46 -0700

    refactor(loading): move defaults from markup into css
    Hardcoded utility classes and inline border color made the spinner
    impossible to restyle; defaults now live in the component variables.

--- src/components/loading/index.ts
+++ src/components/loading/index.ts
@@ -2,13 +2,6 @@ import { html, Attributes } from '@esportsplus/template';
 import './scss/index.scss';
 
 
-export default (attributes?: Attributes) => {
-    return html`
-        <div
-            class='loading --border-width-700 --size-800'
-            style='--border-color: var(--color-border-500);'
-            ${attributes}
-        >
-        </div>
-    `;
-};
+export default (attributes?: Attributes) => html`
+    <div class='loading' ${attributes}></div>
+`;
